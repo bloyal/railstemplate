e07063e8de2a3c45b467b45210290c5fa409ad6c
Brian Loyal <bloyal@gmail.com>
2013-11-25 21:30:27 -0500

    Initial rail template commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railstemplate::Application.config.secret_token = 'b2dd407762cedaad0a92462fdb371e469c3f17dd4e7891120918ce5afe936a32d04b308a798198fe3f31d4fbf2b8b43f3f39b809d1c7958b7d1c1ed607362245'
